9ff75e680c75bf38459f58f019df89f62f179bb3
azzar budiyanto <48999412+1999AZZAR@users.noreply.github.com>
2020-09-23 14:46:42 +0700

    Add files via upload

---
+++ vbfiles/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Main
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub L01INPUTOUTPUTToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles L01INPUTOUTPUTToolStripMenuItem.Click
+        L01_18518241046.Show()
+    End Sub
+
+    Private Sub L02SevenSegmentToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles L02SevenSegmentToolStripMenuItem.Click
+        L02.Show()
+    End Sub
+End Class
